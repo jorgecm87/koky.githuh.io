d82c94f85637aea064eb8134f383b4919d2b9191
jorgecm87 <44273116@certus.edu.pe>
2025-05-06 22:03:34 -0500

    clase 06/05

--- SCRIPT1.js
+++ SCRIPT1.js
@@ -79,20 +79,62 @@ function FAgregarTextoPermanente()
 
  function FCapturarNombre(){
 
-    const vTexto1 = document.getElementById("nombre");
-    const vTexto2 = document.querySelectorAll("input.datosPersonales");
+    const vTexto1 = document.getElementById("humberto");
+    const vTexto2 = document.querySelectorAll("input.datospersonales");
+    const vTexto3 = document.getElementById("clave");
+
    // usar input para clases relacioandas a cajas de texto, etc
-    vTexto1.style.color="red";
+    vTexto1.style.color="blue";
     
     var valor1 =vTexto1.value; // value obtiene el contenido de la caja de texto nombre
-    var valor2 ="";
-
-    document.getElementById("observacion").value = valor1;//textarea ya captura el nombre
-   //foreach recorre todo el contenido del array de la clase datosPersonales
+    var valor2 =""; //tipo string 
+    vTexto3.value=valor1; // copia el texto1 como clave
+    
+  
+    //foreach recorre todo el contenido del array de la clase datosPersonales
     vTexto2.forEach(input => {
-        valor2 =valor2 + `${input.name}: ${input.value}\n`;
+        valor2 =valor2 +  `${input.name}: ${input.value} ${input.id} \n`;  // alt+96 
+        input.style.border="2px solid orange" // da color al borde 
+      //para comillas tipotexto permiten colocar variables del form y convertirlas a texto mediante $
       });
+      vTexto2[1].style.border="2px solid blue" // dar color al borde del texto nombrado en este caso 2
 
-    document.getElementById('observacion').value = valor2;
+    document.getElementById("observacion").value = valor2;
     console.log('El valor del nombre es:', valor1);
+}
+function FUsoFor1()
+{
+  const vTextoClass = document.getElementsByClassName("classFOR1");
+  var vTextoTotal="";
+  // como length=4 --> ira desde 0 hasta 3
+  for(let i=0 ; i< vTextoClass.length ; i++ )
+  {
+    vTextoClass[i].style.border ="2px solid red" ;
+    vTextoTotal=vTextoTotal + vTextoClass[i].innerText+'-';
+
+  } // fin de for
+  var nuevoH1 =document.createElement("h1");
+  var texto = document.createTextNode(vTextoTotal);
+  nuevoH1.appendChild(texto);
+  vTextoClass[3].appendChild(nuevoH1);
+  
+}  // fin de function
+
+function FEliminarConcat()
+{
+  const vTextoClass = document.getElementsByClassName("classFOR1");
+  let rpta =prompt("Estas seguro de eliminar el texto concatenado?(S/N)",'');
+  if (rpta=="S" || rpta=="s")  //  doble || significa o
+    { vTextoClass[3].remove();  // eliminar la class de posicion [3]
+      alert("CONCATENCION ELMINADA");
+    }  
+  else
+    { if(rpta=="N" || rpta=="n")
+      { alert("NO SE elimnará");
+      }
+      else
+      { alert("OPCION INCORRECTA-INgresa nuevamente");
+      }
+    }
+  //removeChild funciona cuando un nodo(class, id, etx) esta dentro de otro
 }
